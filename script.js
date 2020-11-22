884bc7881a40c040eda06cd84127ba42adffa30a
testdevity <48357576+testdevity@users.noreply.github.com>
2020-11-22 16:01:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var $ = function(id) {
+  return document.getElementById(id);
+};
+var inc = 0;
+var out = 0;
+var str = 'devity.tk';
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+var t;
+
+var anim = function() {
+  inc++;
+  if (inc % 50 === 0 && out < str.length) {
+    $('anim').appendChild(document.createTextNode(str[out]));
+    out++;
+  } else if (out >= str.length) {
+    $('shuffle').innerHTML = '';
+    removeInterval(t);
+  }
+  $('shuffle').innerHTML =
+    chars[Math.floor(Math.random() * chars.length)];
+};
+t = setInterval(anim, 10);
+$('anim').innerHTML = '';
+
+//Do it again
+function reload() {
+  window.location.href = window.location.href;
+}
+document.getElementById('btn').onclick = reload;
